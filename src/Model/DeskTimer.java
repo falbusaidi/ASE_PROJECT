591d6ed5e3591f904b0cd16e62fcfb3822a94978
Aravand94 <jo2@hw.ac.uk>
2018-03-08 12:13:31 +0000

    Add files via upload

---
+++ src/Model/DeskTimer.java
@@ -0,0 +1,39 @@
+package Model;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class DeskTimer
+{	
+	private DeskManager SetupDesks;
+	Timer timer= new Timer();
+	public DeskTimer(DeskManager getDeskManager, long closeDeskTime)
+	{
+		//Get settings for desk manager
+		this.SetupDesks=getDeskManager;
+		
+		//System.out.println("Debug 1");
+		//Initialize schedule to setup desks
+		timer.schedule(new TimerTask() {
+			@Override
+			public void run()
+			{
+				SetupDesks.OpenDesks();
+				//System.out.println("Debug a");				
+			}
+		}, 5*1000);
+		
+		//Initialize schedule to close the desks
+		timer.schedule(new TimerTask() {
+			@Override
+			public void run()
+			{
+				SetupDesks.closeAllDesks();
+				//System.out.println("Debug b");
+			}
+		}, closeDeskTime);
+		
+			
+	}
+		
+}
